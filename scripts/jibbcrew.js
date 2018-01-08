8cd7bbf0dd79b7440971fb8a036dcfe5cf291208
nicholas will <nicholaswill@payrollcenter.com>
2018-01-08 18:25:13 -0500

    hotfix - nav menu fix

--- scripts/jibbcrew.js
+++ scripts/jibbcrew.js
@@ -1,3 +1,11 @@
+$(document).on('click','.navbar-collapse.in',function(e) {
+
+    if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
+        $(this).collapse('hide');
+    }
+
+});
+
 var post_type = 'all';
 
 //router
--- scripts/manager.js
+++ scripts/manager.js
@@ -3,6 +3,14 @@ $(document).ready(function() {
     checkLogin();
 });
 
+$(document).on('click','.navbar-collapse.in',function(e) {
+
+    if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
+        $(this).collapse('hide');
+    }
+
+});
+
 function addPost()
 {
 
